fa35dc76e39787f1a1afe5132f4c485475fe07be
mohan0611 <mohan0611@gmail.com>
2016-08-20 21:39:05 +0530

    Implemented the modal form

--- AngularAndMVC/App/Customer/ViewModels/CustomerListViewModel.js
+++ AngularAndMVC/App/Customer/ViewModels/CustomerListViewModel.js
@@ -1,4 +1,4 @@
-﻿customerModule.controller("customerListViewModel", function ($scope, customerService, $http, $q, $routeParams, $window, $location, viewModelHelper, $uibModal) {
+﻿customerModule.controller("customerListViewModel", function ($scope, customerService, $http, $q, $routeParams, $window, $location, viewModelHelper, $uibModal, $log) {
 
     $scope.viewModelHelper = viewModelHelper;
     $scope.customerService = customerService;
@@ -27,5 +27,59 @@
 
    
     initialize();
+
+
+
+    //Modal test
+
+    
+    $scope.animationsEnabled = true;
+
+    $scope.open = function (size) {
+        //console.log(size);
+        var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                item: function () {
+                    return size;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    $scope.toggleAnimation = function () {
+        $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
 });
 
+
+customerModule.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, item) {
+
+    $scope.item = item;
+
+    //$scope.items = items;
+
+    //$scope.selected = {
+    //    item: $scope.items[0]
+    //};
+
+    $scope.ok = function () {
+        $uibModalInstance.close($scope.item);
+        console.log($scope.item);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
